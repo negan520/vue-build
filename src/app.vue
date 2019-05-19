040533bbd7e44bd9736ab669a50015889034da4b
negan <330639009@qq.com>
2019-05-19 23:01:21 +0800

    配置js和css打包

--- src/app.vue
+++ src/app.vue
@@ -8,6 +8,14 @@
     export default {
         data() {
             return {
+                activeName: '首页',//侧滑菜单选中项
+                noticeList: [],//公告数据列表
+                index: 0,//判断侧滑菜单选中项
+                egDate: '',//首页日期时间
+                imgIdex:0,
+                scrollNotice:0,
+                selfLeft:0.5,
+                noticeIn:null,
                 text: 'lrxu'
             }
         }
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require("path");//nodejs里面的基本包，用来处理路径
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
+const ParallelUglifyPlugin=require('webpack-parallel-uglify-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 //__dirname表示文件相对于工程的路径
 module.exports ={
@@ -17,24 +20,62 @@ module.exports ={
             filename: 'index.html',
             template: 'index.html',
             inject: 'body'
-        })
+        }),
+        new ExtractTextPlugin({
+            filename: "style/index.css?[chunkhash]"
+        }),
+        new CleanWebpackPlugin(),
     ],
-    mode:'none',
+    mode:'production',
     module: {
         rules: [
             {//通过vue-loader来识别以vue结尾的文件
                 test: /.vue$/,
                 loader: 'vue-loader'
             },
-            {//通过vue-loader来识别以vue结尾的文件
-                test: /.css$/,
-                //css的处理方式不同，有嵌入在页面style标签里的，有从外部文件引入的，我们这里用use来声明
-                use: [
-                    'style-loader',//接受潜在页面内部的style标签的文件。
-                    'css-loader'
-                ]
+            {
+                test: /.js$/,
+              //  include: [__dirname + 'dist'],
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['es2015'],
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader'],
+                    filename:'[name].min.css'
+                })
             }
-        ]
+        ],
+    },
+    optimization: {
+        minimizer: [
+            new ParallelUglifyPlugin({
+                // 传递给 UglifyJS 的参数
+                uglifyJS: {
+                    output: {
+                        // 最紧凑的输出
+                        beautify: false,
+                        // 删除所有的注释
+                        comments: false,
+                    },
+                    compress: {
+                        // 在UglifyJs删除没有用到的代码时不输出警告
+                        warnings: false,
+                        // 删除所有的 `console` 语句，可以兼容ie浏览器
+                        drop_console: true,
+                        // 内嵌定义了但是只用到一次的变量
+                        collapse_vars: true,
+                        // 提取出出现多次但是没有定义成变量去引用的静态值
+                        reduce_vars: true,
+                    }
+                },
+            }),
+        ],
     }
 }
 if (isDev) {
